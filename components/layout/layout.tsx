6fe920553194ee273c5a50bf3801bffc416f47b2
Rafael Kepler <rafael.kepler@hotmail.com>
2026-03-25 11:59:31 -0300

    included a hamburger menu

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,5 +1,8 @@
 import { ReactNode } from "react";
 import { Toaster } from "../ui/sonner";
+import { SidebarHeader, SidebarMenu, SidebarMenuItem, SidebarProvider, useSidebar } from "../ui/sidebar";
+import { Sidebar } from "lucide-react";
+import SidebarComponent from "../sidebar/sidebar";
 
 interface LayoutComponentProps {
     children: ReactNode
@@ -9,9 +12,17 @@ export default function LayoutComponent({children} : LayoutComponentProps) {
     return (
         <>
             <div className="min-h-screen bg-linear-to-br from-slate-100 to-slate-200 flex items-center justify-center p-6"> 
-                {children}
-            </div>
-            <Toaster />
+                    {children}
+                </div>
+                <Toaster />
+                    <SidebarProvider style={
+                        {
+                        "--sidebar-width": "20rem",
+                        "--sidebar-width-mobile": "20rem",
+                        } as React.CSSProperties
+                    }>
+                <SidebarComponent />
+            </SidebarProvider>
         </>
     )
 }
---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { 
+    Sidebar, 
+    SidebarContent, 
+    SidebarHeader, 
+    SidebarMenu, 
+    SidebarMenuButton, 
+    SidebarMenuItem,
+    useSidebar 
+} from "../ui/sidebar";
+
+import { menuItems } from "@/utils/menu-items";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function SidebarComponent() {
+
+    const { open, setOpen, toggleSidebar } = useSidebar();
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const handleSidebarClick = (url: string) => {
+        router.push(url);
+        setOpen(false);
+    }
+
+
+    return (
+        <>
+            <div className="fixed top-4 left-4 z-50 md:hidden">                
+                <button onClick={toggleSidebar} className="p-2 rounded-md hover:bg-muted transition">
+                    <Image 
+                        alt="logo da jumeuc" 
+                        src="/logo_juve_correto.png" 
+                        width={30} 
+                        height={30} 
+                    />
+                </button>                
+            </div>
+
+            <Sidebar collapsible="icon" className="z-40">
+                <SidebarHeader>
+                    <SidebarContent 
+                        onClick={toggleSidebar}
+                        className="cursor-pointer"                        
+                    >
+                        <Image 
+                            alt="logo da jumeuc" 
+                            src="/logo_juve_correto.png" 
+                            width={30} 
+                            height={30} 
+                        />
+                    </SidebarContent>
+
+                    <SidebarMenu hidden={!open}>
+                        <SidebarMenuItem>
+
+                            <SidebarMenuButton
+                                variant="outline"
+                                onClick={() => handleSidebarClick("/")}
+                                className="flex items-center justify-center cursor-pointer"
+                                tooltip="Volte para o menu inicial"
+                                isActive={pathname === "/"}
+                            >
+                                Início
+                            </SidebarMenuButton>
+
+                            {menuItems.map((menuItem) => (
+                                <SidebarMenuButton
+                                    key={menuItem.title}
+                                    onClick={() => handleSidebarClick(menuItem.path)}
+                                    tooltip={menuItem.description}
+                                    variant="outline"
+                                    className="flex items-center justify-center cursor-pointer"
+                                    isActive={pathname === menuItem.path}
+                                >
+                                    {menuItem.title}
+                                </SidebarMenuButton>
+                            ))}
+
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarHeader>
+            </Sidebar>
+        </>
+    )
+}
