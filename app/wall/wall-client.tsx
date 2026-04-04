7935fd81c0e8e85d17b0d02a9b227bc0bf1d2762
Rafael Kepler <rafael.kepler@hotmail.com>
2026-04-04 15:02:06 -0300

    including table on wall

--- app/wall/wall-client.tsx
+++ app/wall/wall-client.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
-import { motion } from "framer-motion";
-import { getAllAttendees } from "@/lib/db";
+import { useEffect, useMemo, useState, useTransition } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { getAllAttendees, getMoreAttendance } from "@/lib/db";
 import { generateAvatar } from "@/utils/wall-utils";
+import { TopAttendanceReturnProp } from "../attendance/types";
+import ListSkelletonComponent from "@/components/list-skelleton/list-skelleton";
+import AttendanceTableComponent from "@/components/attendance-table/attendance-table";
+import Image from "next/image";
 
 export default function WallClient() {
     const [users, setUsers] = useState<string[]>([]);
+    const [isWhatVisible, setIsWhatVisible] = useState<number>(1);
+    const [mostAttendance, setMostAttendance] = useState<TopAttendanceReturnProp[]>([]);
+    const [ isPending, startTransition ] = useTransition();
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        const defineWhatWillBeVisible : Record<string, () => void> = {
+            "ArrowRight": () => setIsWhatVisible(prev => prev < 2 ? prev + 1 : prev),
+            "ArrowLeft": () => setIsWhatVisible(prev => prev > 0 ? prev - 1 : prev)
+        }
+        defineWhatWillBeVisible[e.key]()
+    }
     
     const userPositions = useMemo(() => {
         return users.map((name) => ({
@@ -19,10 +34,9 @@ export default function WallClient() {
     }));
 }, [users]);
 
-    useEffect(() => {
+    useEffect(() => {        
         const fetchUsers = async () => {
             const usersUpdated = await getAllAttendees()
-
             setUsers((prev) => {
                 if (usersUpdated.length !== prev.length) {
                     return usersUpdated
@@ -30,23 +44,62 @@ export default function WallClient() {
                 return prev
             })
         }
+        const fetchAttendees = async () => {
+            const attendance = await getMoreAttendance();
+            setMostAttendance(attendance);
+        }
 
         const interval = setInterval(fetchUsers, 2000)
 
         fetchUsers()
+        startTransition(() => {
+            fetchAttendees();
+        })
 
-        return () => clearInterval(interval)
+        window.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            clearInterval(interval)
+            window.removeEventListener("keydown", handleKeyDown)
+        }
     }, [])
 
     return (
         <div className="relative min-h-screen bg-gradient-to-br from-red-900 yellow to-green-900 overflow-hidden">
-            <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-                <img
-                    src="/logo_sem_fundo.png"
-                    alt="Logo"
-                    className="w-124 opacity-80"
-                />
-            </div>
+            <AnimatePresence mode="wait">
+                <motion.div
+                    key={isWhatVisible}
+                    initial={{ x: 300, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    exit={{ x: 600, opacity: 0 }}
+                    transition={{ duration: 0.4, ease: "easeInOut" }}
+                    className="absolute inset-0 flex items-center justify-center"
+                >
+                    {isWhatVisible === 1 && (
+                    <img
+                        src="/logo_sem_fundo.png"
+                        alt="Logo"
+                        className="w-124 opacity-80"
+                    />
+                    )}
+
+                    {isWhatVisible === 0 && (
+                    <div className="rounded-2xl w-1/3 h-1/2 bg-card p-10 flex items-center justify-center z-10">
+                        <Image src="/qrcode.png" alt="QR Code image" width={400} height={400} />
+                    </div>
+                    )}
+
+                    {isWhatVisible === 2 && (
+                    <div className="rounded-2xl w-1/2 h-2/3 bg-card p-10 z-10 flex justify-center items-center">
+                        {isPending ? (
+                        <ListSkelletonComponent />
+                        ) : (
+                        <AttendanceTableComponent topAttendants={mostAttendance}/>
+                        )}
+                    </div>
+                    )}
+                </motion.div>
+            </AnimatePresence>
             <h1 className="absolute top-6 w-full text-center text-4xl font-bold text-white z-10">
                 A galera tá chegando 🏃
             </h1>
--- components/list-skelleton/list-skelleton.tsx
+++ components/list-skelleton/list-skelleton.tsx
@@ -2,97 +2,28 @@ import { motion } from "framer-motion";
 
 export default function ListSkelletonComponent() {
     return (
-        <>
-            <div
-                className="relative overflow-hidden bg-slate-200 rounded-xl h-8 mb-2"
-                >
-                <motion.div
-                    className="absolute inset-0"
-                    animate={{
-                        x: ["-100%", "100%"]
-                    }}
-                    transition={{
-                        repeat: Infinity,
-                        duration: 1.2,
-                        ease: "linear"
-                    }}
-                    style={{
-                        background: "linear-gradient(90deg, transparent, rgba(255,255,255,0.4), transparent)"
-                    }}
-                />
-            </div>
-            <div
-                className="relative overflow-hidden bg-slate-200 rounded-xl h-8 mb-2"
-                >
-                <motion.div
-                    className="absolute inset-0"
-                    animate={{
-                        x: ["-100%", "100%"]
-                    }}
-                    transition={{
-                        repeat: Infinity,
-                        duration: 1.2,
-                        ease: "linear"
-                    }}
-                    style={{
-                        background: "linear-gradient(90deg, transparent, rgba(255,255,255,0.4), transparent)"
-                    }}
-                />
-            </div>
-            <div
-                className="relative overflow-hidden bg-slate-200 rounded-xl h-8 mb-2"
-                >
-                <motion.div
-                    className="absolute inset-0"
-                    animate={{
-                        x: ["-100%", "100%"]
-                    }}
-                    transition={{
-                        repeat: Infinity,
-                        duration: 1.2,
-                        ease: "linear"
-                    }}
-                    style={{
-                        background: "linear-gradient(90deg, transparent, rgba(255,255,255,0.4), transparent)"
-                    }}
-                />
-            </div>
-            <div
-                className="relative overflow-hidden bg-slate-200 rounded-xl h-8 mb-2"
-                >
-                <motion.div
-                    className="absolute inset-0"
-                    animate={{
-                        x: ["-100%", "100%"]
-                    }}
-                    transition={{
-                        repeat: Infinity,
-                        duration: 1.2,
-                        ease: "linear"
-                    }}
-                    style={{
-                        background: "linear-gradient(90deg, transparent, rgba(255,255,255,0.4), transparent)"
-                    }}
-                />
-            </div>
-            <div
-                className="relative overflow-hidden bg-slate-200 rounded-xl h-8 mb-2"
-                >
-                <motion.div
-                    className="absolute inset-0"
-                    animate={{
-                        x: ["-100%", "100%"]
-                    }}
-                    transition={{
-                        repeat: Infinity,
-                        duration: 1.2,
-                        ease: "linear"
-                    }}
-                    style={{
-                        background: "linear-gradient(90deg, transparent, rgba(255,255,255,0.4), transparent)"
-                    }}
-                />
-            </div>
-        </>
+        <div className="flex justify-center flex-col w-full">
+            {[...Array(5)].map((_, i) => (
+                <div
+                    className="relative overflow-hidden bg-slate-200 rounded-xl h-8 mb-2 w-full"
+                    key={i}
+                    >
+                    <motion.div
+                        className="absolute inset-0 z-10" 
+                        animate={{
+                            x: ["-100%", "100%"]
+                        }}
+                        transition={{
+                            repeat: Infinity,
+                            duration: 1.2,
+                            ease: "linear"
+                        }}
+                        style={{
+                            background: "linear-gradient(90deg, transparent, rgba(255,255,255,0.4), transparent)"
+                        }}
+                    />
+                </div>
+            ))}
+        </div>
     )
 }
